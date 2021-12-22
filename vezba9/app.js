c5526104137cdbbffceb9af37f834372d2926959
darishodzic <daris.hodzic.dh@gmail.com>
2021-12-22 19:58:26 +0100

    commit

---
+++ vezba9/app.js
@@ -0,0 +1,79 @@
+// brojGodina = prompt("Unesi koliko imas godina");
+
+// console.log(brojGodina);
+
+// console.log(1 > 5);
+// console.log(7 > 5);
+// console.log(5 >= 5);
+// console.log(4 <= 5);
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+// console.log("D" === "D");
+// console.log("D" === "D");
+
+// console.log(Boolean(0));
+// console.log(Boolean(5));
+
+// console.log(Boolean(""));
+// console.log(Boolean("ASDFGHJKL"));
+
+// var1 = true;
+
+// var2 = false;
+
+// null
+
+// console.log(Boolean(NaN));
+
+// && AND
+
+// || OR
+
+// false || false === false
+// true || false === true
+// true || true ===true
+// false||true === true
+
+// true && true === true;
+// false && false === false;
+// false && true === false;
+// true && false === false;
+
+// a = 5;
+// b = 10;
+
+// console.log(a < b);
+
+// a = prompt("Unesi broj izmedju 1 i 10");
+
+// console.log(a < 10 && a > 1 );
+
+// console.log(true || (false && true));
+
+// a = prompt("Unesi broj ");
+
+// console.log(a % 2 === 0);
+
+// a = 5;
+
+// b = 3;
+
+// c = a === b;
+
+// console.log(c);
+
+// console.log("Daris".length);
+
+// a = "Daris";
+// console.log(a.length);
+
+// ime = prompt("Unesi ime");
+// duzina = ime.length;
+
+// console.log("Duzina vaseg imena je : ", duzina);
+
+// ime = "Daris";
+//     01234
+// console.log(ime[5]);
