23c6859e2852a73c406c35fa63bb624959760775
darishodzic <daris.hodzic.dh@gmail.com>
2021-12-24 20:00:25 +0100

    commit

--- vezba9/app.js
+++ vezba9/app.js
@@ -77,3 +77,80 @@
 // ime = "Daris";
 //     01234
 // console.log(ime[5]);
+
+// a = "Daris";
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.concat("Nesto"));
+// console.log("Darisa".indexOf("a"));
+// console.log("Darisa".includes("g"));
+
+// str = "Apple, Banana, Kiwi";
+// part = str.slice(7, 13);
+
+// console.log(part);
+
+// str = "Apple, Banana, Kiwidsgfhghkjlkl";
+// part = str.slice(7);
+
+// console.log(part);
+
+// text = "Please visit Microsoft!";
+
+// newText = text.replace("Microsoft", "W3Schools");
+
+// console.log(newText);
+
+// text = "kiwi,banana,daris,kiwi";
+
+// console.log(text.split(","));
+
+// a = 5 + 2;
+
+// console.log("Ovaj broj je", a, "i on je u stringu");
+// console.log(`Ovaj broj je ${a} i on je u stringu`);
+
+// a = Math.random() * 100;
+
+// console.log(Math.round(a));
+
+// if (5 > 2) {
+//   console.log("tacno je");
+// }
+
+// console.log("ostali code");
+// console.log("ostali code");
+
+// a = 150;
+// b = 20;
+
+// if (a > b) {
+//   console.log("A je vece");
+// }
+
+// if (b > a) {
+//   console.log("B je vece");
+// }
+
+// a = prompt("Unesi broj 1-10: ");
+
+// if (a > 1 && a < 10) {
+//   console.log("Unos je tacan");
+// }
+
+// if (a < 1 || a > 10) {
+//   console.log("Unos je POGRESAN");
+// }
+
+// if (5 > 1) {
+//   console.log("IF JE TACAN");
+// } else {
+//   console.log("IF SE NIJE IZVRSI,IZVRSIO SE ELSE");
+// }
+
+//-----------------------------------------
+//Domaci:
+//Napisati program da proveri da li je uneti string "dupliran",EXAMPLE:
+// "darisdaris"
+// console.log("daris" === "daris");
+//-----------------------------------------
