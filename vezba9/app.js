712eeec11a0f9303465b267087a0b9550a06db5b
daris <daris.hodzic.dh@gmail.com>
2022-01-26 20:03:55 +0100

    commit

--- vezba9/app.js
+++ vezba9/app.js
@@ -811,3 +811,115 @@
 // }
 
 //==============================================================
+
+// a = 5;
+// b = 5;
+
+// console.log(a === b);
+
+// a = [1, 2, 3];
+// b = [1, 2, 3];
+
+// console.log(a === b);
+
+// a = [1, 2, 3];
+
+// b = a;
+
+// a.pop();
+
+// console.log(b);
+
+// a = 5;
+// b = a;
+// a = 4;
+// console.log(b);
+
+// a = [3, 6, 23, 9, 44, 14, 83, 22, 5, 1];
+// sum = 0;
+// for (i = 0; i < a.length; i++) {
+//   sum += a[i];
+// }
+// console.log(sum / a.length);a = [3, 6, 23, 9, 44, 14, 83, 22, 5, 1];
+// sum = 0;
+// for (i = 0; i < a.length; i++) {
+//   sum += a[i];
+// }
+// console.log(sum / a.length);
+
+//==============================================
+
+// arr = [3, 6, 0, 23, 0, 9, 44, 14, 0, 83, 22, 5, 0, 1];
+
+// for (i = 0; i < arr.length; i++) {
+//   if (arr[i] === 0) {
+//     arr.splice(i, 1);
+//     arr.push(0);
+//   }
+// }
+
+// console.log(arr);
+
+//==============================================
+
+// arr = [3, 6, 23, 9, 44, 14, 0, 83, 22, 5];
+
+// min = arr[0];
+// max = arr[0];
+
+// for (el of arr) {
+//   if (el > max) {
+//     max = el;
+//   } else if (min > el) {
+//     min = el;
+//   }
+// }
+
+// console.log("Razlika je: ", max - min);
+
+//==============================================
+
+// arr = [3, 6, 23, 9, 44, 14, 0, 83, 22, 5];
+
+// sadrzi = false;
+
+// for (el of arr) {
+//   if (el === 67 || el === 87) {
+//     sadrzi = true;
+//   }
+// }
+
+// console.log(sadrzi);
+
+//==============================================
+
+// arr = [3, 6, 23, 9, 44, 14, -254, 0, 83, 22, 5];
+
+// max = arr[0];
+
+// for (el of arr) {
+//   if (Math.abs(el) > Math.abs(max)) {
+//     max = el;
+//   }
+// }
+
+// console.log(max);
+
+//==============================================
+
+arr = [3, 6, 23, 9, 44, 14, -254, 0, 83, 22, 5];
+min1 = arr[0];
+min2 = arr[0];
+
+for (el of arr) {
+  if (el < min1) {
+    min1 = el;
+  } else if (el < min2 && el > min1) {
+    min2 = el;
+  }
+}
+
+console.log("Najmanji je", min1);
+console.log("drugi manji je", min2);
+
+//==============================================
